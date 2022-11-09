f427a58b996ce1b713786daf23c4920cda41ca16
User <User@DESKTOP-37U4KKJ>
2022-11-09 20:10:14 -0300

    Agregar archivos de proyecto.

---
+++ calculadoraProject/Form1.cs
@@ -0,0 +1,205 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace calculadoraProject
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        double a;
+        double b;
+        string c;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "")
+            {
+                textBox1.Text = "1";
+            }
+            else
+            {
+                textBox1.Text = textBox1.Text + 1;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "")
+            {
+                textBox1.Text = "2";
+            }
+            else
+            {
+                textBox1.Text = textBox1.Text + 2;
+            }
+
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "")
+            {
+                textBox1.Text = "4";
+            }
+            else
+            {
+                textBox1.Text = textBox1.Text + "4";
+            }
+        }
+
+        private void button14_Click(object sender, EventArgs e)
+        {
+            b = Convert.ToDouble(this.textBox1.Text);
+            switch (c)
+            {
+                case "+":
+                    this.textBox1.Text = Convert.ToString(b + a);
+                    break;
+                case "-":
+                        this.textBox1.Text = Convert.ToString(a - b);
+                    
+                    break;
+                case "*":
+                    this.textBox1.Text = Convert.ToString(b * a);
+                    break;
+                case "/":
+                        this.textBox1.Text = Convert.ToString(a / b);
+                    
+                    break;
+
+            }
+        }
+
+        private void button13_Click(object sender, EventArgs e)
+        {
+            a = Convert.ToDouble(this.textBox1.Text);
+            c = "+";
+            this.textBox1.Clear();
+            this.textBox1.Focus();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "")
+            {
+                textBox1.Text = "3";
+            }
+            else
+            {
+                textBox1.Text = textBox1.Text + 3;
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "")
+            {
+                textBox1.Text = "5";
+            }
+            else
+            {
+                textBox1.Text = textBox1.Text + 5;
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "")
+            {
+                textBox1.Text = "6";
+            }
+            else
+            {
+                textBox1.Text = textBox1.Text + 6;
+            }
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "")
+            {
+                textBox1.Text = "7";
+            }
+            else
+            {
+                textBox1.Text = textBox1.Text + 7;
+            }
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "")
+            {
+                textBox1.Text = "8";
+            }
+            else
+            {
+                textBox1.Text = textBox1.Text + 8;
+            }
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "")
+            {
+                textBox1.Text = "9";
+            }
+            else
+            {
+                textBox1.Text = textBox1.Text + 9;
+            }
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "")
+            {
+                textBox1.Text = "0";
+            }
+            else
+            {
+                textBox1.Text = textBox1.Text + 0;
+            }
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            a = Convert.ToDouble(this.textBox1.Text);
+            c = "/";
+            this.textBox1.Clear();
+            this.textBox1.Focus();
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            a = Convert.ToDouble(this.textBox1.Text);
+            c = "*";
+            this.textBox1.Clear();
+            this.textBox1.Focus();
+        }
+
+        private void button15_Click(object sender, EventArgs e)
+        {
+            a = Convert.ToDouble(this.textBox1.Text);
+            c = "-";
+            this.textBox1.Clear();
+            this.textBox1.Focus();
+        }
+
+        
+    }
+}
